6ad506b6f7765e296c08a8d2fdfe8a0f2a98dab4
Padeen <paulo.deen@gmail.com>
2019-05-26 19:21:06 +0200

    Add Projects form, Add create + save action to projectscontroller

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Create a new project</h1>    
+    <form method="POST" action="/projects">
+        {{ csrf_field () }}
+        <div>
+            <input type="text" name="title" placeholder="Project title">
+        </div>
+        <div>
+            <textarea name="description" placeholder="Project description"></textarea>
+        </div>
+        <div>
+            <button type="submit">Create project</button>
+        </div>
+    </form>
+</body>
+</html>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>Projects</h1>    
+
+    @foreach ($projects as $project)
+        <li>{{ $project->title }}</li>
+
+    @endforeach
 </body>
 </html>
